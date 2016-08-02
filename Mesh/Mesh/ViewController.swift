aabb4374cbe26bc171ccace8a9d26d3c59b9948c
Chris Truman <cleetruman@gmail.com>
2016-08-02 15:24:00 -0700

    Initial Commit

---
+++ Mesh/Mesh/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  Mesh
+//
+//  Created by Christopher Truman on 8/2/16.
+//  Copyright © 2016 Tinder. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
